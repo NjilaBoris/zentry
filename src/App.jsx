1a9685cc62bc84b7e8b6606d08376fae500f8f5d
NjilaBoris <njilaboris2@gmail.com>
2025-06-19 00:19:49 +0100

    Implement Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import FloatingImage from "./components/Story";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <FloatingImage />
+      <Contact />
     </main>
   );
 };
